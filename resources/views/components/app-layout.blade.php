945c132b7c2bc41f1b9d61a26c45bee9ea0051ed
xonixsit <xonixsitsolutions@gmail.com>
2025-06-02 08:54:35 -0700

    cors2

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -17,7 +17,6 @@
     @else
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <script src="{{ mix('/js/app.js') }}" defer></script>
-
     @endenv
 </head>
 <body class="font-sans antialiased">
