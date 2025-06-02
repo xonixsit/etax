fda7beaa37e88d6d518a8ee4649bf6e8c2bc75ff
xonixsit <xonixsitsolutions@gmail.com>
2025-06-02 08:33:18 -0700

    cors

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -12,7 +12,13 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @env('local')
+      @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    @endenv
 </head>
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
